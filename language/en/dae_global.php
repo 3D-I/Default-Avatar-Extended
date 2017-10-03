294194e131ac965a60336dcb98df8c7148f40e40
3Di <three3di@hotmail.it>
2017-10-03 06:06:05 +0200

    Language change

--- language/en/dae_global.php
+++ language/en/dae_global.php
@@ -19,7 +19,7 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'DEFAULT_AVATAR'				=>	'Default avatar',
+	'DEFAULT_AVATAR'				=>	'DAE avatar',
 	// Translators please do not change the following line, no need to translate it!
 	'DAE_CREDIT_LINE'	=>	' | <a href="https://github.com/3D-I/Default-Avatar-Extended">Default Avatar Extended</a> &copy; 2017 - 3Di',
 ));
